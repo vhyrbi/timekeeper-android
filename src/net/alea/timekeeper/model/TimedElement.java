c6339689a6c4dbfb0f186382538d61c441aea4f2
vhyrbi <vhyrbi@alea>
2012-12-11 22:49:47 +0100

    Add: class that associates a person and a chronometer

---
+++ src/net/alea/timekeeper/model/TimedElement.java
@@ -0,0 +1,39 @@
+/*
+TimeKeeper - Software to monitor how much time each person speaks
+Copyright (C) 2012 Vincent Hiribarren
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package net.alea.timekeeper.model;
+
+public class TimedElement {
+
+	private final String _name;
+	private final Chrono _chrono;
+	
+	public TimedElement(String name, Chrono chrono) {
+		_name = name;
+		_chrono = chrono;
+	}
+	
+	public String getName() {
+		return _name;
+	}
+	
+	public Chrono getChrono() {
+		return _chrono;
+	}
+	
+}
